97f3a71169e7c3e9d8920ed7a33cd1422e5d1b65
cristi16 <patrascu.cristinel@gmail.com>
2015-04-28 22:55:22 +0200

    color assignment

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,26 +10,57 @@ public class GameController : MonoBehaviour
     public Text nickname;
     public GameObject loginUI;
     public GameObject playerInfoUI;
-
     public static Color PlayerColor;
+
+    private Dictionary<int, int> takenColorIndices;
+
+    private PhotonView view;
+
+    void Start()
+    {
+        view = GetComponent<PhotonView>();
+        takenColorIndices = new Dictionary<int, int>();
+    }
 
     public void Connect()
     {
+        PhotonNetwork.player.name = GameObject.FindGameObjectWithTag("PlayerName").GetComponent<UnityEngine.UI.Text>().text;
         PhotonNetwork.ConnectUsingSettings("0.1");     
     }
 
-    public void AssingPlayerName(string name)
+    public IEnumerator Initialize()
     {
-        PhotonNetwork.player.name = name;
-        var pickedColor = assignableColors[PhotonNetwork.playerList.Length];
+        while (takenColorIndices.Count != PhotonNetwork.playerList.Length - 1)
+            yield return null;
+
+        var pickedColor = Color.white;
+        for(int i = 0; i < assignableColors.Count; i++)
+        {
+            if (takenColorIndices.ContainsValue(i))
+                continue;
+            else
+            {
+                pickedColor = assignableColors[i];
+                view.RPC("MarkAssignedColor", PhotonTargets.AllBuffered, i, PhotonNetwork.player.ID);
+                break;
+            }
+        }
+        assignableColors.RemoveAt(0);
         PlayerColor = pickedColor;
         playerInfoUI.SetActive(true);
         loginUI.SetActive(false);
-        nickname.text += " " + name;
+        nickname.text += " " + PhotonNetwork.player.name;
         colorImage.color = pickedColor;
 
         var dot = PhotonNetwork.Instantiate("PlayerDot", Vector3.zero, Quaternion.identity, 0);
         dot.transform.position = this.transform.position + transform.forward * (Camera.main.nearClipPlane + 2f);       
+    }
+
+    [RPC]
+    void MarkAssignedColor(int index, int playerID)
+    {
+        Debug.Log("marking color index: " + index + " for player: " + playerID);
+        takenColorIndices.Add(playerID, index);
     }
 
     void OnJoinedLobby()
@@ -40,7 +71,13 @@ public class GameController : MonoBehaviour
 
     void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
     {
-        Debug.Log("New Player Connected: " + newPlayer.name);
+        Debug.Log("New Player Connected: " + newPlayer.name + "[" + newPlayer.ID + "]");
+    }
+
+    void OnPhotonPlayerDisconnected(PhotonPlayer oldPlayer)
+    {
+        takenColorIndices.Remove(oldPlayer.ID);
+        Debug.Log("New Player Disconnected: " + oldPlayer.name + "[" + oldPlayer.ID + "]");
     }
 
     void OnPhotonRandomJoinFailed()
@@ -51,6 +88,6 @@ public class GameController : MonoBehaviour
     void OnJoinedRoom()
     {
         Debug.Log("Joined room");
-        AssingPlayerName(GameObject.FindGameObjectWithTag("PlayerName").GetComponent<UnityEngine.UI.Text>().text);
+        StartCoroutine(Initialize());
     }
 }
