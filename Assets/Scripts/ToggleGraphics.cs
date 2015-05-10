fc99ac77b93a0b31180b88781e0f1c00965b38b1
cristi16 <patrascu.cristinel@gmail.com>
2015-05-10 11:34:22 +0200

    ui stuff

---
+++ Assets/Scripts/ToggleGraphics.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Toggle))]
+[RequireComponent(typeof(Image))]
+public class ToggleGraphics : MonoBehaviour
+{
+    public Sprite onGfx;
+    public Sprite offGfx;
+    private Toggle toggle;
+    private Image image;
+
+    void Start()
+    {
+        toggle = GetComponent<Toggle>();
+        image = GetComponent<Image>();
+        image.sprite = toggle.isOn ? onGfx : offGfx;
+    }
+
+    public void UpdateGfx()
+    {
+        image.sprite = toggle.isOn ? onGfx : offGfx;
+    }
+    
+}
