6e2c879009e03ed805a3a62d4ecbfdd3c508cb98
cristi16 <patrascu.cristinel@gmail.com>
2015-06-08 12:28:39 +0200

    tweaking graphics

--- Assets/Scripts/ToggleGraphics.cs
+++ Assets/Scripts/ToggleGraphics.cs
@@ -7,14 +7,26 @@ using System.Collections;
 public class ToggleGraphics : MonoBehaviour
 {
     public GizmoControllerCS.GIZMO_MODE mode;
-    public Sprite onGfx;
-    public Sprite offGfx;
+    public Sprite onGfxWindows;
+    public Sprite offGfxWindows;
+    public Sprite onGfxMac;
+    public Sprite offGfxMac;
     private Toggle toggle;
     private Image image;
     private GizmoControllerCS gizmoController;
+    private Sprite onGfx;
+    private Sprite offGfx;
 
     void Start()
     {
+#if UNITY_STANDALONE_OSX
+        onGfx = onGfxMac;
+        offGfx = offGfxMac;
+#else
+        onGfx = onGfxWindows;
+        offGfx = offGfxWindows;
+#endif
+
         toggle = GetComponent<Toggle>();
         image = GetComponent<Image>();
         image.sprite = toggle.isOn ? onGfx : offGfx;
